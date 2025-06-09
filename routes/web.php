42c7c1ba97333cbb7f550944c92717bb429ce0f7
kristiani060704 <kristianipakpahan09@gmail.com>
2025-06-09 18:16:55 +0700

    api.php

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,12 @@ Route::get('/profile', function () {
 Route::get('/absensi', function () {
     return view('absensi');
 });
+
+use App\Http\Controllers\NoteController;
+
+Route::get('/notes', [NoteController::class, 'index']);
+Route::post('/notes', [NoteController::class, 'store']);
+Route::get('/notes/{id}', [NoteController::class, 'show']);
+Route::put('/notes/{id}', [NoteController::class, 'update']);
+Route::delete('/notes/{id}', [NoteController::class, 'destroy']);
+
